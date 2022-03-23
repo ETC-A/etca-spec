dcbf74ce0627bce80147a0419a95a0cd3af841a1
Max Kopinsky <zerglingk9012@gmail.com>
2022-03-22 22:39:47 -0500

    Add simple store-load test

---
+++ etc-semantics/test/store_load.s
@@ -0,0 +1,15 @@
+;-mstrict
+;16
+.extension byte_operations
+
+    movx %rx1,0
+    movx %rx0,0x0aff
+    movx [%rx1],%rx0
+    movx %rx2,[%rx1]
+    movh %rh3,[%rh1]
+    addx %rx1,1
+    movh %rh4,[%rh1]  
+    movh [%rh1],1    ; note 1: not word-aligned
+                     ; note 2: config checks *0 unchanged
+end:
+    jmp  end
---
+++ etc-semantics/test/store_load.sh
@@ -0,0 +1,2 @@
+set -eu
+./scripts/test.sh "$1" "$2" "$3" "test/store_load.bin" 16
